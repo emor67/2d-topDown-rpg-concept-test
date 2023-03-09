48145b87a73a378daaa88e23deb2c236d4008643
emir çakır <emir_cakir.67@hotmail.com>
2023-03-09 17:20:29 +0300

    Player movement is now functional. I have some issues with sourcetree about size of unstaged files and I am gonna solve them asap.

---
+++ 2dTopDownRPGConceptTest/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private Transform movePoint;
+    [SerializeField] private LayerMask wall;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.fixedDeltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
+        {
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f,wall)){
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, wall))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+        //rb.velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed * Time.fixedDeltaTime;
+    }
+}
