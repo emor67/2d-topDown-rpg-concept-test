3f0bf315faa4cc7172747b1c2cfb46ab2b7bbfc0
emir çakır <emir_cakir.67@hotmail.com>
2023-03-10 18:27:59 +0300

    Action system delayed.

--- Assets/Scripts/ActionSystem.cs
+++ Assets/Scripts/ActionSystem.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class ActionSystem : MonoBehaviour
 {
-    private void Update()
-    {
-    
-    }
+ 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
